c8d15f3410cf5d5fd63029c5ea050ffa8634e26f
AlexJrDev <alexjrdev@gmail.com>
2022-11-07 12:32:07 -0600

    player basic controls

---
+++ Assets/Scripts/Managers/GamePlayManager/ConstantsManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JamOff.Scripts.Managers
+{
+
+    public class ConstantsManager : MonoBehaviour
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Player/Player_MovementController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_MovementController : MonoBehaviour
+{
+    [SerializeField] float speedMovement;
+    [SerializeField] float jumpForce;
+    float moveInput;
+    bool facingRight = true;
+
+
+
+    public bool isGrounded;
+    public LayerMask whatisGround;
+    [SerializeField] int jumps;
+    int resetJumps;
+
+    Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        resetJumps = jumps;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            MakeJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        playerRb.velocity = new Vector2(moveInput * speedMovement, playerRb.velocity.y);
+
+        if (needToFlip())
+        {
+            Flip();
+        }
+    }
+
+
+    void MakeJump()
+    {
+        if (jumps > 0)
+        {
+            isGrounded = false;
+            playerRb.velocity = Vector3.up * jumpForce;
+            jumps--;
+        }
+    }
+
+    public void ResetJumps()
+    {
+        isGrounded = true;
+        jumps = resetJumps;
+    }
+
+
+
+
+
+
+
+    bool needToFlip()
+    {
+        if ((facingRight == false && moveInput > 0) || facingRight == true && moveInput < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
